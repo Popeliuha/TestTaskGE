64306ffd446b5b406b70d3dc85c75ca04153e1a3
popeliuha <popelyshkon25@gmail.com>
2023-05-14 01:02:45 +0300

    Test project

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using NLog;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Database;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string ReadText(string text)
+        {
+            try
+            {
+                IWebDriver driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://google.com");
+                IWebElement txtSearch = driver.FindElement(By.Name("q"));
+                txtSearch.SendKeys(text + Keys.Enter);
+
+                string searchResults = "Результаты поиска"; //if it fails from your browser, replace content of this string to "Search results" in your language
+                string firstResultXPath = $"//h1[text()='{searchResults}']/following-sibling::div/div[1]";
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+                IWebElement firstResult = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(firstResultXPath)));
+                IWebElement firstResultName = firstResult.FindElement(By.XPath("//h3"));
+                string firstResultsText = firstResultName.Text;
+                logger.Info(firstResultsText);
+                driver.Close();
+                using (var context = new TestContext())
+                {
+                    var entity = new TestEntitie { SavedText = firstResultsText};
+                    context.TestEntitiesDB.Add(entity);
+                    context.SaveChanges();
+                }
+                return firstResultsText;
+            }
+            catch(Exception e)
+            {
+                logger.Error(e.Message);
+                logger.Error(e.StackTrace);
+                return e.Message;
+            }
+        }
+    }
+}
---
+++ WebApplication1/Database/TestContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Database
+{
+    public class TestEntitie
+    {
+        public int Id { get; set; }
+        public string SavedText { get; set; }
+    }
+    public class TestContext : DbContext
+    {
+        public TestContext()
+            : base(@"Server=DESKTOP-0OQGPH0\SQLEXPRESS;Database=TestDB;Trusted_Connection=True;")
+        {
+        }
+
+        public virtual DbSet<TestEntitie> TestEntitiesDB { get; set; }
+    }
+}
